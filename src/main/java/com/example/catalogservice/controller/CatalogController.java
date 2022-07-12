ba614c9792b11eb9ef7afc29b07593d02acb7cb6
oksk1111 <oksk1111@gmail.com>
2022-07-12 17:59:06 +0900

    catalog service - done

---
+++ src/main/java/com/example/catalogservice/controller/CatalogController.java
@@ -0,0 +1,42 @@
+package com.example.catalogservice.controller;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.service.CatalogService;
+import com.example.catalogservice.vo.ResponseCatalog;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog-service")
+@RequiredArgsConstructor
+public class CatalogController {
+    private final Environment env;
+    private final CatalogService catalogService;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's working in catalog service on port %s",
+                env.getProperty("local.server.port"));
+    }
+
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getUsers() {
+        Iterable<CatalogEntity> catalogList = catalogService.getAllCatalogs();
+
+        List<ResponseCatalog> result = new ArrayList<>();
+        catalogList.forEach(v -> {
+            result.add(new ModelMapper().map(v, ResponseCatalog.class));
+        });
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
